70f0bf81abbb1a58e0ce7e786c4771b9e2f76c0e
Bramwell Osayande <bramwellosayande@gmail.com>
2022-07-19 04:03:38 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("advanced node")
+console.log("advanced node");
+const cowsay = require("cowsay");
+console.log(cowsay.say({
+    text:"aloha",
+    wrapLength:24,
+    mode:'p'
+
+}
+    ))
